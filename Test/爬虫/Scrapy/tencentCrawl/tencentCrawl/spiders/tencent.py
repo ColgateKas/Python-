c58bb86ae51455d83e5124c56ca4e2050af6b453
CologateKas <351883791@qq.com>
2017-10-31 13:38:48 +0800

    Crawl模式爬取并记录日志

---
+++ Test/爬虫/Scrapy/tencentCrawl/tencentCrawl/spiders/tencent.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from tencentCrawl.items import TencentcrawlItem
+
+
+class TencentSpider(CrawlSpider):
+    name = 'tencent'
+    allowed_domains = ['tencent.com']
+    start_urls = ['http://hr.tencent.com/position.php?&start=0#a']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'start=\d+'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        each_line = response.xpath('//tr[@class="even"]|//tr[@class="odd"]')
+        for each in each_line:
+            item = TencentcrawlItem()
+            #职位名称
+            item['positionName'] = each.xpath("./td[1]/a/text()").extract_first()
+            #职位链接
+            item['positionLink'] = each.xpath("./td[1]/a/@href").extract_first()
+            #职位类别
+            item['positionType'] = each.xpath("./td[2]/text()").extract_first()
+            #招聘人数
+            item['positionPeple'] = each.xpath("./td[3]/text()").extract_first()
+            #工作地点
+            item['workLocation'] = each.xpath("./td[4]/text()").extract_first()
+            #发布时间
+            item['publishTime'] = each.xpath("./td[5]/text()").extract_first()
+
+            yield item
